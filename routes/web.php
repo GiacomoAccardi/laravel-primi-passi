c0d3ad0a91cb3250670b071b329649bf8efc83f1
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-23 16:59:43 +0200

    [UPD] There's now a dynamic message on the page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $message = 'Mi sa che Laravel mi piace';
+
+    return view('welcome', compact('message'));
 });
